ff7b9de434decab6e265c9d376e8d95e1ca83fd8
Nambokle <135355990+Nambokle@users.noreply.github.com>
2024-04-25 20:57:14 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+token = '' #โทเคนบอท
+webhook = 'https://discord.com/api/webhooks/1230912846909411500/O5P7rWIxQvZgIa3Ye5hITs-bKim7_XDkncsOrclWbxEWHLId_HUYFpAzrdazZoo12qQ3' #ลิ้งค์เว็ปฮุคแจ้งเตือนเวลามีคนมาซื้อของ
+topup_log = 'https://discord.com/api/webhooks/1230912846909411500/O5P7rWIxQvZgIa3Ye5hITs-bKim7_XDkncsOrclWbxEWHLId_HUYFpAzrdazZoo12qQ3' #ลิ้งค์เว็ปฮุคแจ้งเตือนเติมเงิน
+phone = '' #เบอร์วอเลทไว้รับเงิน
+
+guild_id = 1230907652620423279 #ไอดีเซิฟเวอร์
+channel_id = 1230912809139834890 #ไอดีช่องส่งงาน
